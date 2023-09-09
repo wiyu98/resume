7e43a8a659cd94a776953e699f2bb783b85e4280
Wilson Yu <wilson@Wilsons-MacBook-Pro.local>
2023-09-09 17:27:26 -0400

    basic app with connections

---
+++ app/models/resumeData.tsx
@@ -0,0 +1,10 @@
+export interface ResumeData {
+    contact: Contact;
+    summary: string;
+}
+
+export interface Contact {
+    email: string;
+    location: string;
+    linkedin: string;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        charcoal: '#272727',
+        sand: '#F5F5DC'
+      },
     },
   },
   plugins: [],
